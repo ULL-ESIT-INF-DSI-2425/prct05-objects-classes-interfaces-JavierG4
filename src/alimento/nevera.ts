9fa79e70cd0326c998d3ce15c3b62a6b5ef5faed
Javier Gonzalez <alu0101548197@ull.edu.es>
2025-02-27 11:24:08 +0000

    final

--- src/alimento/nevera.ts
+++ src/alimento/nevera.ts
@@ -1,7 +1,126 @@
 import { Bebida, Alimento } from "./alimento_bebida";
 
+/**
+ * Clase nevera
+ */
 export class Nevera {
-  constructor(private Alimentos: [Alimento, number][], private Bebida) {}
+  /**
+   * 
+   * @param Alimentos - Lista de alimentos y la cantidad
+   * @param Bebidas - Lista de bebidas y su cantidas
+   */
+  constructor(private Alimentos: [Alimento, number][], private Bebidas: [Bebida, number][]) {}
 
+  /**
+   * Constructor de la clase
+   * @returns Devuelve una lista de alimentos y bebidas que hay en la nevera
+   */
+  consultarListado():(Alimento| Bebida)[] {
+    const cadena :  (Alimento| Bebida)[] = []
+    this.Alimentos.forEach(alimento => {
+      if(alimento[1] > 0) {
+        cadena.push(alimento[0])
+      }
+    })
+    this.Bebidas.forEach(bebida => {
+      if(bebida[1] > 0) {
+        cadena.push(bebida[0])
+      }
+    });
+    return cadena
+  }
 
+  /**
+   * FUncion que añade o quita un elemento de la nevera
+   * @param añadidoQuitado - Elemento a añadir o a quitar 
+   * @param flag - true = añade, false = quita
+   * @returns devuelve una lista de elemtnos disponibles despues de la modificacion
+   */
+  añadirQuitarComida(añadidoQuitado: [Alimento,number], flag: boolean):(Alimento)[] {
+    const valores : Alimento[] = []
+    let contador = 0
+    this.Alimentos.forEach((alimento,index) => {
+      if(alimento[0].id === añadidoQuitado[0].id) {
+        if(flag === true) {
+          this.Alimentos[index][1] = alimento[1] + añadidoQuitado[1]
+          contador = 1
+        } else {
+          this.Alimentos[index][1] = this.Alimentos[index][1] - añadidoQuitado[1]
+          contador = 1
+        }
+
+      }
+    })
+    if (contador === 0 && flag === true){
+      this.Alimentos.push(añadidoQuitado)
+    }
+    this.Alimentos.forEach(comida => {
+      if(comida[1]>0) {
+        valores.push(comida[0])
+      }
+    })
+
+    return valores
+  }
+  /**
+   * Funcion que añade o quita
+   * @param añadidoQuitado - Elemento a añadir o quitar
+   * @param flag - true = añade, false = quita
+   * @returns devuelve una lista de elementos disponibles despues de la modificacion
+   */
+
+  añadirQuitarBebida(añadidoQuitado: [Bebida,number], flag: boolean):(Bebida)[] {
+    const valores : Bebida[] = []
+    let contador = 0
+    this.Bebidas.forEach((bebida,index) => {
+      if(bebida[0].id === añadidoQuitado[0].id) {
+        if(flag === true) {
+          this.Bebidas[index][1] = bebida[1] + añadidoQuitado[1]
+          contador = 1
+        } else {
+          this.Bebidas[index][1] = this.Bebidas[index][1] - añadidoQuitado[1]
+          contador = 1
+        }
+
+      }
+    })
+    if (contador === 0 && flag === true){
+      this.Bebidas.push(añadidoQuitado)
+    }
+    this.Bebidas.forEach(bebida => {
+      if(bebida[1]>0) {
+        valores.push(bebida[0])
+      }
+    })
+
+    return valores
+  }
+
+  /**
+   * Funcion que lista las cosas que hay que comprar, es decir, que no hay 
+   * @returns Devuelve lista de bebidas que hay que comprar
+   */
+  listadoBebidasCompra(): Bebida[] {
+    const cadena: Bebida[] = []
+    this.Bebidas.forEach(bebida => {
+      if (bebida[1] <= 0) {
+        cadena.push(bebida[0])
+      }
+    } )
+    return cadena
+  }
+
+/**
+   * Funcion que lista las cosas que hay que comprar, es decir, que no hay 
+   * @returns Devuelve lista de bebidas que hay que comprar
+   */
+  listadoAlimentoCompra(): Alimento[] {
+    const cadena: Alimento[] = []
+    this.Alimentos.forEach(alimento => {
+      if (alimento[1] <= 0) {
+        cadena.push(alimento[0])
+      }
+    })
+    return cadena
+  }
 }
--- tests/practica.spec.ts
+++ tests/practica.spec.ts
@@ -1,9 +1,14 @@
 import { describe, test, expect } from "vitest";
 import { Bebida, Alimento} from "../src/alimento/alimento_bebida";
+import{ Nevera} from "../src/alimento/nevera"
 
 const Alimento1: Alimento = new Alimento(10,"pollo", 20, "Muy saludable", 150, 200)
-//const Alimento2: Alimento = new Alimento(15,"merluza", 10, "Poco saludable", 110, 240)
+const Alimento2: Alimento = new Alimento(15,"merluza", 10, "Poco saludable", 110, 240)
+const Alimento3: Alimento = new Alimento(20,"carne", 10, "Poco saludable", 110, 240)
 const Bebida1: Bebida = new Bebida(13, "Coca-Cola", 5, "poco saludable", 300, "Magenesio, hierro y azucares")
+const alimentos: [Alimento, number][] = [[Alimento1, 0], [Alimento2, 5]]
+const bebidas: [Bebida, number][] = [[Bebida1, 5]]
+const Nevera1: Nevera = new Nevera(alimentos, bebidas)
 
 describe("Test de clase Alimento", ()=> {
   test("Get id", ()=> {
@@ -62,5 +67,27 @@ describe("Test de clase Bebida", ()=> {
 
   test("Showinfo", ()=> {
     expect(Bebida1.showInfo()).toEqual([13, "Coca-Cola", 5, "poco saludable", 300, "Magenesio, hierro y azucares"])
+  })
+})
+
+describe ("Nevera", ()=> {
+  test("Consultar listado", ()=> {
+    expect(Nevera1.consultarListado()).toEqual([Alimento2, Bebida1])
+  })
+
+  test("Añadir comida",() => {
+    expect(Nevera1.añadirQuitarComida([Alimento3, 5], true)).toEqual([Alimento2, Alimento3])
+  })
+
+  test("Añadir bebida", () => {
+    expect(Nevera1.añadirQuitarBebida([Bebida1,5], false)).toEqual([])
+  })
+
+  test("Lista de compra comida",() =>  {
+    expect(Nevera1.listadoAlimentoCompra()).toEqual([Alimento1])
+  })
+
+  test("Lista de compra bebidas", ()=> {
+    expect(Nevera1.listadoBebidasCompra()).toEqual([Bebida1])
   })
 })
